07c8783a832a5f25986d32b61e9a1afe8b597777
Antoine Duvauchelle <antoine@duvauchelle.co>
2021-01-21 21:45:47 +0100

    commented slugs

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,45 +1,45 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Blog.module.css'
-import { BLOGPOST_QUERY, request } from '../lib'
-import { Layout, PageContainer } from '../components'
+// import Head from 'next/head'
+// import Link from 'next/link'
+// import styles from '../styles/Blog.module.css'
+// import { BLOGPOST_QUERY, request } from '../lib'
+// import { Layout, PageContainer } from '../components'
 
-export const getStaticProps = async () => {
-  const data = await request({
-    query: BLOGPOST_QUERY,
-    variables: { limit: 10 }
-  });
-  return { props: { data } }
-}
+// export const getStaticProps = async () => {
+//   const data = await request({
+//     query: BLOGPOST_QUERY,
+//     variables: { limit: 10 }
+//   });
+//   return { props: { data } }
+// }
 
 
-export default function Collectif({data}) {
-  const {allPosts} = data
+// export default function Collectif({data}) {
+//   const {allPosts} = data
 
-  console.log(allPosts)
+//   console.log(allPosts)
 
-  return (
-    <Layout>
-      <Head>
-        <title>Collectif Béranger | Parents d'élèves</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>Collectif Béranger | Parents d'élèves</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <PageContainer>
-        <h1 className={styles.title}>Blog</h1>
-        <div className={styles.postsGrid}>
-          {allPosts.map((post, i) => (
-            <Link key={i} href={`/${post.slug}`}>
-              <a>
-                <img src={post.images[0].url} style={{height: '280px', objectFit: 'cover', width: '100%'}} />
-                <h4 className={styles.parentName}>{post.titre}</h4>
-                <p>{post.updatedAt}</p>
-                <p>{post.excerpt}</p>
-              </a>
-            </Link>
-          ))}
-        </div>
-      </PageContainer>
-    </Layout>
-  )
-}
+//       <PageContainer>
+//         <h1 className={styles.title}>Blog</h1>
+//         <div className={styles.postsGrid}>
+//           {allPosts.map((post, i) => (
+//             <Link key={i} href={`/${post.slug}`}>
+//               <a>
+//                 <img src={post.images[0].url} style={{height: '280px', objectFit: 'cover', width: '100%'}} />
+//                 <h4 className={styles.parentName}>{post.titre}</h4>
+//                 <p>{post.updatedAt}</p>
+//                 <p>{post.excerpt}</p>
+//               </a>
+//             </Link>
+//           ))}
+//         </div>
+//       </PageContainer>
+//     </Layout>
+//   )
+// }
